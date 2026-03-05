3cc23475112ef6ede59dddc542d874ee30c0874d
Bruna Sales <144288264+brunasalesdesousa@users.noreply.github.com>
2026-03-05 16:28:18 -0300

    first face

---
+++ java-curso/estruturas-basicas-java/estruturas-basicas-java/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        // <tipo> identificador = valor;
+        /**Aqui podemos contar por mais de uma linha, dando enter no final ela abre mais linhas 
+         * Sintaxe de declaração das variáveis no Java
+         * <tipo> identificador = valor;
+         * <tipo> identificador = valor;
+         * Variáveis tem escopos: blocos de comandos no qual a variável foi declarada.
+        */
+        String saudacao = "Olá, Bru";
+        System.out.println(saudacao);
+    }
+}
