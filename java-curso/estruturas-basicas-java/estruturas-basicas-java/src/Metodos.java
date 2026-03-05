c734e6f49c094c3daaa6ba7ab6dda5bd9c5d39da
Bruna Sales <144288264+brunasalesdesousa@users.noreply.github.com>
2026-03-05 17:20:54 -0300

    testando métodos

---
+++ java-curso/estruturas-basicas-java/estruturas-basicas-java/src/Metodos.java
@@ -0,0 +1,22 @@
+public class Metodos {
+    public static void main(String[] args) {
+      // ativarSoneca();   
+      String mensagemDoAlarme = agendarAlarme(12, 30); // Argumentos
+      System.out.println(mensagemDoAlarme);
+    }
+
+    /**
+     * Sintaxe de declaração de métodos no Java:
+     * <tipo do retorno do metodo> identificador(<tipo> parâmetros) {
+     * <bloco de comandos>
+     * }
+     */
+    static void ativarSoneca() {
+      System.out.println("Só mais 5 minutinhos :P");
+    }
+
+    static String agendarAlarme(int hora, int minutos) { // parâmetros 
+        // Seu alarme foi configurado às 12:30
+       return "Seu alarme foi configurado às " + hora + ":" + minutos;
+    }
+}
